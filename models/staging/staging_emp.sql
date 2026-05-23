a62c9808c29655450b284e5f0b96987742bce20a
aarvi morey <nacor24935@anawebs.com>
2026-05-23 10:34:25 +0000

    new

--- models/staging/staging_emp.sql
+++ models/staging/staging_emp.sql
@@ -1,16 +1,16 @@
-{{config(
-    materialized = 'incremental',
-    incremental_strategy = 'merge',
-    unique_key = 'emp_id'
-)}}
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='emp_id'
+) }}
 
-select emp_id,
- INITCAP(NULLIF(TRIM(emp_name), '')) AS first_name,
- from 
-{{source('yogesh','emp')}}
+SELECT *
+FROM {{ source('yogesh', 'emp') }}
 
 {% if is_incremental() %}
-where joining_date > (
-    select max(joining_date)
-    from {{this}})
-{%endif%}
+WHERE joining_date >
+(
+    SELECT MAX(joining_date)
+    FROM {{ this }}
+)
+{% endif %}
